016510c51abce8375e0d0fe2543e933d8a224980
PawelZ1994 <pawelzelek.kacperek@gmail.com>
2026-08-03 12:43:44 +0000

    add logs

--- temperaturyapi/src/main/java/com/example/temperaturyapi/controller/PomiarController.java
+++ temperaturyapi/src/main/java/com/example/temperaturyapi/controller/PomiarController.java
@@ -6,11 +6,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @CrossOrigin
 public class PomiarController {
 
+    private static final Logger logger = LoggerFactory.getLogger(PomiarController.class);
     private final PomiarRepository repo;
 
     public PomiarController(PomiarRepository repo) {
@@ -20,13 +23,14 @@ public class PomiarController {
     // 📡 GET /dane (jak w Node)
     @GetMapping("/dane")
     public List<Pomiar> getDane() {
+        logger.info("Pobrano listę pomiarów.");
         return repo.findAll();
     }
 
     // ➕ POST /dodaj (jak w Node)
     @PostMapping("/dodaj")
     public String dodaj(@RequestBody Pomiar p) {
-
+        logger.info("Próba dodania pomiaru: {}", p.getTemperatura());
         if (p.getTemperatura() < -100 || p.getTemperatura() > 100) {
             return "Nieprawidlowa temperatura";
         }
